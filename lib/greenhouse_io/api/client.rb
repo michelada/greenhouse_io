b7754f03197f293a0a095d6c168696c175eb7a8c
Jesus Figueroa <jesus.figo29@gmail.com>
2017-02-15 11:56:55 -0800

    Adding last_activity_after filter to permitted options

--- lib/greenhouse_io/api/client.rb
+++ lib/greenhouse_io/api/client.rb
@@ -3,7 +3,7 @@ module GreenhouseIo
     include HTTMultiParty
     include GreenhouseIo::API
 
-    PERMITTED_OPTIONS = [:page, :per_page, :job_id, :tags, :created_after, :updated_after]
+    PERMITTED_OPTIONS = [:page, :per_page, :job_id, :tags, :created_after, :updated_after, :last_activity_after]
 
     attr_accessor :api_token, :rate_limit, :rate_limit_remaining, :link
     base_uri 'https://harvest.greenhouse.io/v1'
